33c18d1f4cc6487b86030954788e74266d024b79
Admin <vangiang260694@gmail.com>
2022-05-26 10:31:50 +0700

    Python Interact To Database SQLite 3

---
+++ main.py
@@ -0,0 +1,89 @@
+import sqlite3
+
+
+def create_table():
+	connection = sqlite3.connect('cinema.db')
+	connection.execute("""
+	CREATE TABLE "Seat" 
+	(
+	"seat_id"	TEXT,
+	"taken"	INTEGER,
+	"price"	REAL
+	);""")
+	connection.commit()
+	connection.close()
+
+
+def insert_record():
+	connection = sqlite3.connect('cinema.db')
+	connection.execute("""
+	INSERT INTO "Seat"
+	(
+	"seat_id",
+	"taken",
+	"price"
+	)
+	VALUES
+	("A1", "0", "90"),
+	("A2", "1", "100"),
+	("A3", "0", "80")
+	""")
+	connection.commit()
+	connection.close()
+
+
+def select_all():
+	connection = sqlite3.connect('cinema.db')
+	cursor = connection.cursor()
+	cursor.execute("""SELECT * FROM "Seat" """)
+	result = cursor.fetchall()
+	connection.close()
+	return result
+
+
+def select_specific_column():
+	connection = sqlite3.connect('cinema.db')
+	cursor = connection.cursor()
+	cursor.execute("""SELECT "seat_id", "price" FROM "Seat" """)
+	result = cursor.fetchall()
+	connection.close()
+	return result
+
+
+def select_with_condition():
+	connection = sqlite3.connect('cinema.db')
+	cursor = connection.cursor()
+	cursor.execute("""SELECT "seat_id", "price" FROM "Seat" WHERE "price" > 80 """)
+	result = cursor.fetchall()
+	connection.close()
+	return result
+
+
+def update_value():
+	connection = sqlite3.connect('cinema.db')
+	connection.execute("""UPDATE "Seat" SET "taken" = 0 WHERE "seat_id" = "A3" """)
+	connection.commit()
+	connection.close()
+
+
+def delete_record():
+	connection = sqlite3.connect('cinema.db')
+	connection.execute("""DELETE FROM "Seat" WHERE "seat_id" = "A3" """)
+	connection.commit()
+	connection.close()
+
+
+def modify_record_by_variable(price, seat_id):
+	connection = sqlite3.connect('cinema.db')
+	connection.execute("""UPDATE "Seat" SET "price" = ? WHERE "seat_id" = ?""", [price, seat_id])
+	connection.commit()
+	connection.close()
+
+
+# insert_record()
+# print(select_all())
+# print(select_specific_column())
+# print(select_with_condition())
+# update_value()
+# delete_record()
+# modify_record_by_variable(price=200, seat_id="A1")
